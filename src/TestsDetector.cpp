ae484ac691f11e19e67c5ebd44d2ea7cfc09301f
mattreturn1 <mattcozz02@gmail.com>
2025-04-28 19:14:40 +0200

    update parameters

--- src/TestsDetector.cpp
+++ src/TestsDetector.cpp
@@ -27,7 +27,7 @@ vector<pair<Rect, string>> detectObjects(
     constexpr int MIN_POINTS_PER_CLUSTER = 18;  // Minimum number of points to consider a valid cluster
     constexpr float BOX_MERGE_DISTANCE = 250.0f;  // Maximum distance between bounding boxes for merging
     const int MIN_BOX_AREA = 2500;  // Minimum allowed area for valid bounding boxes
-    const float DYNAMIC_MARGIN = 0.9f;  // Factor for dynamic margin based on points' standard deviation
+    const float DYNAMIC_MARGIN = 1.0f;  // Factor for dynamic margin based on points' standard deviation
 
     // Preprocess the scene for object detection
     Mat preprocessedScene = preprocessImage(scene);
